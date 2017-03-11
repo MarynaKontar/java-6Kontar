be0ae07b45d7eee2cd31d5a702dbb3d8cb8647a3
Maryna Kontar <marina.acoustic@gmail.com>
2017-03-11 13:48:12 +0200

    module6 unique method

--- src/module6/task2_3/UserUtils.java
+++ src/module6/task2_3/UserUtils.java
@@ -18,7 +18,7 @@ import java.util.Arrays;
  */
 public final class UserUtils {              //nobody can inherit UserUtils
 
-    private UserUtils() {                   //cannot create an instance of this class
+    private UserUtils () {                   //cannot create an instance of this class
     }
 
     /**
@@ -27,25 +27,27 @@ public final class UserUtils {              //nobody can inherit UserUtils
      * @param users
      * @return
      */
-    public static User[] uniqueUsers(User[] users) {  //НЕ ДОДЕЛАЛА
-        User[] unique = new User[users.length];
-//        int k = 0;
-//        boolean ifUnique = false;
-//        for (int i = 0; i < users.length; i++) {
-//            for (int j = i + 1; j < users.length; j++) {
-//                if (users[i] == null || !(users[i].equals(users[j]))) { //если нулл или одинаковые
-//                    ifUnique = true;
-//                }
-//            }
-//            if (ifUnique){k++;}
-//            unique[k] = users[i];
-//        }
-
-
+    public static User[] uniqueUsers ( User[] users ) {
+            User[] unique = new User[ users.length ];
+            int k = 0;
+            int n;
+            for ( int i = 0; i < users.length; i++ ) {
+                // ifUnique[ i ] = true;
+                n = 0;
+                for ( int j = i + 1; j < users.length; j++ ) {
+                    if (users[ i ] == null || (users[ i ].equals ( users[ j ] ))) {
+                        n++;
+                    }
+                }
+                if (n == 0) {
+                    unique[ k ] = users[ i ];
+                    k++;
+                }
+            }
         //TODO 5.Если бы массив был массивом чисел, то его можно было бы отсортировать квиксортом и одним for
         //проверять на одинаковость соседнии элементы. А как в этом случае сделать поиск одинаковых (уникальных) элементов быстрее?
         //Comparable?
-        //unique = Arrays.copyOf(unique, k);
+        unique = Arrays.copyOf ( unique, k - 1 );
         return unique;
     }
 
@@ -56,12 +58,12 @@ public final class UserUtils {              //nobody can inherit UserUtils
      * @param balance
      * @return
      */
-    public static User[] usersWithContitionalBalance(User[] users, int balance) {
-        User[] usersWithContitionalBalance = new User[0];
-        for (int i = 0; i < users.length; i++) {
-            if (users[i] == null) continue;
-            else if (users[i].getBalance() == balance)
-                usersWithContitionalBalance = addElementToArray(usersWithContitionalBalance, users[i]);
+    public static User[] usersWithContitionalBalance ( User[] users, int balance ) {
+        User[] usersWithContitionalBalance = new User[ 0 ];
+        for ( int i = 0; i < users.length; i++ ) {
+            if (users[ i ] == null) continue;
+            else if (users[ i ].getBalance ( ) == balance)
+                usersWithContitionalBalance = addElementToArray ( usersWithContitionalBalance, users[ i ] );
         }
         return usersWithContitionalBalance;
     }
@@ -73,12 +75,13 @@ public final class UserUtils {              //nobody can inherit UserUtils
      * @param users
      * @return
      */
-    public final static User[] paySalaryToUsers(User[] users) {
-        User[] usersWithSalary = new User[users.length];
-        for (int i = 0; i < users.length; i++) {
-            if (users[i] == null) continue;
-            else usersWithSalary[i] = new User(users[i].getId(), users[i].getFirstName(), users[i].getLastName(),
-                    users[i].getSalary(), users[i].getBalance() + users[i].getSalary());
+    public final static User[] paySalaryToUsers ( User[] users ) {
+        User[] usersWithSalary = new User[ users.length ];
+        for ( int i = 0; i < users.length; i++ ) {
+            if (users[ i ] == null) continue;
+            else
+                usersWithSalary[ i ] = new User ( users[ i ].getId ( ), users[ i ].getFirstName ( ), users[ i ].getLastName ( ),
+                        users[ i ].getSalary ( ), users[ i ].getBalance ( ) + users[ i ].getSalary ( ) );
             //TODO 6. т.к. класс User неизменяемый (immutable), то я не могу изменить баланс конкретному пользователю,
             //а должна создать нового пользователя с теми же значениями полей, но измененным балансом. Все верно?
         }
@@ -95,11 +98,11 @@ public final class UserUtils {              //nobody can inherit UserUtils
      * @param users
      * @return
      */
-    public final static long[] getUsersId(User[] users) {
-        long[] usersId = new long[users.length];
-        for (int i = 0; i < users.length; i++) {
-            if (users[i] == null) continue;
-            else usersId[i] = users[i].getId();
+    public final static long[] getUsersId ( User[] users ) {
+        long[] usersId = new long[ users.length ];
+        for ( int i = 0; i < users.length; i++ ) {
+            if (users[ i ] == null) continue;
+            else usersId[ i ] = users[ i ].getId ( );
         }
         return usersId;
     }
@@ -111,31 +114,59 @@ public final class UserUtils {              //nobody can inherit UserUtils
      * @param users
      * @return
      */
-    public static User[] deleteEmptyUsers(User[] users) {
-        User[] deletedEmptyUsers = new User[users.length];
+    public static User[] deleteEmptyUsers ( User[] users ) {
+        User[] deletedEmptyUsers = new User[ users.length ];
         int n = 0;
-        for (int i = 0; i < users.length; i++) {
-            if (users[i] != null) {
-                deletedEmptyUsers[n] = users[i];
+        for ( int i = 0; i < users.length; i++ ) {
+            if (users[ i ] != null) {
+                deletedEmptyUsers[ n ] = users[ i ];
                 n++;
             }
         }
-        deletedEmptyUsers = Arrays.copyOf(deletedEmptyUsers, n);
+        deletedEmptyUsers = Arrays.copyOf ( deletedEmptyUsers, n );
         return deletedEmptyUsers;
     }
 
 
-    private static User[] addElementToArray(User[] userArray, User newUser) {
-        userArray = Arrays.copyOf(userArray, userArray.length + 1);
-        userArray[userArray.length - 1] = newUser;
+    private static User[] addElementToArray ( User[] userArray, User newUser ) {
+        userArray = Arrays.copyOf ( userArray, userArray.length + 1 );
+        userArray[ userArray.length - 1 ] = newUser;
         return userArray;
     }
-
-    //TODO 7. Получается, что новые массивы я создавала двумя методами:
-    // 1. создавала массив такой же длины,что и исходный (т.к. знала, что он точно не будет большего размера).
-    // Добавляла в него нужные элементы в цикле, а потом обрезала лишнюю "концовку"  (uniqueUsers, deleteEmptyUsers)
-    // 2. создавала массив нулевой длины, а в цикле добавляла в него нужные элементы с помощью, т. е.
-    // каждый раз создавая новый массив (usersWithContitionalBalance)
-    // ВОПРОС: 1 метод лучше использовать, когда длина исходного массива не очень велика,
-    // а второй метод - когда количество добавляемых элементов не очень много?????
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//TODO 7. Получается, что новые массивы я создавала двумя методами:
+// 1. создавала массив такой же длины,что и исходный (т.к. знала, что он точно не будет большего размера).
+// Добавляла в него нужные элементы в цикле, а потом обрезала лишнюю "концовку"  (uniqueUsers, deleteEmptyUsers)
+// 2. создавала массив нулевой длины, а в цикле добавляла в него нужные элементы с помощью addElementToArray, т. е.
+// каждый раз создавая новый массив (usersWithContitionalBalance)
+// ВОПРОС: 1 метод лучше использовать, когда длина исходного массива не очень велика,
+// а второй метод - когда количество добавляемых элементов не очень много?????
+
+// 7. Ты немного не понимаешь, как устроен массив. Его длинна фиксирована. Когда ты создаёшь массив с длинной 0,
+// то ты не можешь туда добавлять элементы. При каждом добавлении нового элемента ты в любом случае пересоздаешь новый массив с длинной +1.
+// Оптимальным решением является понять сколько элементов будет добавлено\удалено из массива, а после этого создать
+// массив уже окончательных размеров куда и поместить все нужные элементы.
+    /*
+     Т.е. лучше делать
+ public final static User[] paySalaryToUsers ( User[] users ) {
+        User[] usersWithSalary = new User[ users.length ];
+
+потом добавлять нужные элементы, и обрезать в конце всеоставщиеся null
+ Так только два раза создается новій массив, а при создании массива нулевой длині в начале, новій массив будет
+ создаваться вплоть до users.length раз
+ */
