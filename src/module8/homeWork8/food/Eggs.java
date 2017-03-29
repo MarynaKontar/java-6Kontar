20dc37d7c29d454790543cd6c07881a4c575beb5
Maryna Kontar <marina.acoustic@gmail.com>
2017-03-29 17:09:33 +0300

    module 8 task1-6 РАБОЧАЯ ВЕРСИЯ

--- src/module8/homeWork8/food/Eggs.java
+++ src/module8/homeWork8/food/Eggs.java
@@ -5,7 +5,7 @@ import module8.homeWork8.Country;
 /**
  * Created by User on 26.03.2017.
  */
-public class Eggs extends Food implements StoredInTheRefrigerator ,Comparable<Food>{
+public class Eggs extends Food implements StoredInTheRefrigerator {
 
     public Eggs ( String name, Country country, int expiration ) {
         super ( name, country, expiration );
