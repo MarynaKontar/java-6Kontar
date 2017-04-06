1fa05d886f67e4545666e468d27ddbee6828326b
Maryna Kontar <marina.acoustic@gmail.com>
2017-04-06 17:36:59 +0300

    module 10 home work task 1-5

---
+++ src/module10/homeWork10/MyNullPointerException.java
@@ -0,0 +1,7 @@
+package module10.homeWork10;
+
+/**
+ * Created by User on 06.04.2017.
+ */
+public class MyNullPointerException extends NullPointerException {
+}
---
+++ src/module10/homeWork10/task5.java
@@ -0,0 +1,34 @@
+package module10.homeWork10;
+
+/**
+ * Java#6
+ * Module 10 Task 5
+ * Create three new types of exceptions. Write a class with a method that throws all three.
+ * In main( ), call the method but only use a single catch clause that will catch all three types of exceptions.
+ *
+ * @author Kontar Maryna
+ */
+public class task5 {
+    public static void main ( String[] args ) {
+
+        try {
+            throwsTreeExceptions();
+        } catch (Exception e) {
+            System.out.println ("Catch!!!" );
+        }
+    }
+
+    private static void throwsTreeExceptions () throws MyIOException {
+
+        try{
+            throw new MyArithmeticException("1");
+        } catch (MyArithmeticException e){
+            try{
+            throw new MyNullPointerException();
+            } catch (MyNullPointerException e1){
+            }
+        }
+
+        throw new MyIOException("2");
+    }
+}
