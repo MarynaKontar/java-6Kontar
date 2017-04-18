8e29f12af4f3d3fef8c7b85ffe481d6dbccc1dd5
Maryna Kontar <marina.acoustic@gmail.com>
2017-04-18 11:55:24 +0300

    FP

--- src/finalProject/Main.java
+++ src/finalProject/Main.java
@@ -27,4 +27,70 @@ package finalProject;
  *
  */
 public class Main {
+
+    /**
+     *         while (true) {
+
+     System.out.println(
+     "Choose what you want to do and write the appropriate number: \n" +
+     "1. Login \n" +
+     "2. Find hotel by hotel name. \n" +
+     "3. Find hotel by city name. \n" +
+     "4. Find room in hotel. \n" +
+     "5. Create account \n" +
+     "6. \n" +
+     "7. Exit\n");
+
+     Integer input;
+     String str = null;
+     try {
+     br = new BufferedReader(new InputStreamReader(System.in));
+     str = br.readLine(); //throw IOException
+     input = Integer.parseInt(str);//throw NumberFormatException
+     //                Scanner br = new Scanner(System.in);
+     //                input = br.nextInt();
+     //                str = input.toString();
+
+     } catch (Exception e) {
+     System.out.println("You typed something strange: " + str + ". " +
+     "And we want a number from 1 to 7. Please, type again.");
+     continue;
+
+     }
+
+     switch (input) {
+     case 1:
+     ;
+     break;
+     case 2:
+     ;
+     break;
+     case 3:
+     ;
+     break;
+     case 4:
+     ;
+     break;
+     case 5:
+     ;
+     break;
+     case 6:
+     ;
+     break;
+     case 7:
+     System.out.println(" You are leaving our program :(  Come again!");
+     System.exit(0);
+     default: {
+     System.out.println("You typed wrong number: " + input + ". " +
+     "And we want a number from 1 to 7. Please, type again.");
+     continue;
+     }
+
+     }
+     }
+     */
+
+
+
+
 }
